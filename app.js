90838cf549fa8fb6502a0023fb6d042282e812cb
Balam Castro <64442582+ekbalam11@users.noreply.github.com>
2024-07-15 22:00:21 +0200

    lottery-check excercise part 1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
-
+//Cargar todo el JSON y lo ponemos en una variable
+const lottery = require('./data/lottery.json');
 
 // Importar el paquete de terceros que acabamos de instalar. Fijaos que como se encuentra en la carpeta node_modules NO hace falta especificar ninguna ruta (al igual que pasa con los built-in modules)
 const express = require('express');
@@ -18,6 +19,48 @@ app.use(express.urlencoded({ extended: true }));  // Middleware para parsear dat
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 })
+
+app.get('/api/check-date', (req, res) => {
+    //1. Tenemos que informar al endpoint de tipo GET de una fecha en concreto. Usaremos una query string para proveer de esta info.
+    // ¿Qué aspecto va a tener una consulta para el 17 de mayo de 2024?
+    // /api/check-date?date=2024-05-17
+
+    //2. Capturar/extraer el valor parámetro 'data'
+    const date = req.query.date;
+    console.log(date);
+
+    //const {date} = req.query;
+
+    //3. Buscar a ver si hay sorteo para la fecha "date" en el lottery.json
+    //cargar el JSON) require, readFileSync...
+
+    //Miramos  si alguno de los sorteos de lottery.json el campo "draw_date" de alguno de sus objetos incluye el substring de la fecha que estamos buscando
+    const item = lottery.find(raffle => raffle.draw_date.includes(date));
+
+    //4. ¿Qué método de array vaís a usar para la búsqueda? .find
+
+    if (item) {
+
+
+    //5. Suponemos de momento que siempre nos pasan una fecha que existe.
+    //2020-09-25 . Tenemos que devolver un JSON con este formato
+
+    /**
+     * {
+     * "message" : "Draw found",
+     * "winningNumbers" : "20 36 37 48 67 16 02"
+     * }
+     */
+    res.send({
+        message: "Draw found",
+        winningNumbers: item.winning_numbers
+    });
+} else {
+    res.status(404).send({
+        message: "Draw not found for the given date"
+    });
+};
+});
 
 // Levantar el servidor
 app.listen(3000, () => {
